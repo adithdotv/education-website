0c885974375e3515274ce12b0c73864cc115149e
adithdotv <adithadiabd17@gmail.com>
2024-07-06 13:11:15 +0530

    final commit

---
+++ src/components/pages/SupportUs/Support.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Support.css'
+
+function Support() {
+  return (
+    <div className='container'>
+        <div className='support-us-container'>
+            <h2>Support Us</h2>
+            <p>Donate to donation@donate.com</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Support
